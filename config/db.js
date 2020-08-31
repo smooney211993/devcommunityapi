99116d5bd79b87dfc866efc62aad57ec65a7b2dd
smooney211993 <sm.athome21@gmail.com>
2020-08-31 16:05:58 +1000

    created the registration of new users in the user endpoint

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
+      useCreateIndex: true,
     });
     console.log('mongoose db connect');
   } catch (error) {
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../modules/User');
 // route get api/users
 // register user
 //access public
@@ -14,16 +17,46 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const { name, email, password } = req.body;
+    // deconstruct the values from the req.body
     const errors = validationResult(req);
     console.log(req.body);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.json('User route');
+    // if error return the error array
     try {
-    } catch (error) {}
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      //if user return the error message in the same json structure
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+      // gravatar criteria
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      // creates a new user in an object
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      // hashed the password
+      await user.save();
+      res.json('User registered');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json('server error');
+    }
   }
 );
 
