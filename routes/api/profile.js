ea7cef3b4a5b92dfa461299a9a279fd200a384f8
smooney211993 <sm.athome21@gmail.com>
2020-09-01 11:31:20 +1000

    defined get request for users via using the webtoken

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const authToken = require('../../middleware/auth');
+const Profile = require('../../modules/Profile');
+const User = require('../../modules/User');
 
-// route get api/profile
-router.get('/', (req, res) => {
-  res.json('Profile route');
+// route get api/profile/me
+// get current users profile private via jsonwebtoken
+router.get('/me', authToken, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('users', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json('Server error');
+  }
 });
 
 module.exports = router;
+S;
