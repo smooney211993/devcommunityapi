b8fd82195b71e4c38da82237147d5edc04d54bd0
smooney211993 <sm.athome21@gmail.com>
2020-09-02 10:38:30 +1000

    found bug in experience put request, title not being recieved in body

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -158,6 +158,31 @@ router.put(
       .not()
       .isEmpty(),
   ],
-  async (req, res) => {}
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { title, company, from, to, current, description } = req.body;
+    const newExperience = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExperience);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
 );
 module.exports = router;
